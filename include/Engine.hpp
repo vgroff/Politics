1675f4070147aacfd40cf377dff7028fa75f1021
vincent <vg1416@ic.ac.uk>
2020-04-04 02:11:51 +0100

    wxWidgets half set uyp

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -1,15 +1,30 @@
 #pragma once
 
+#include <wx/wx.h>
 #include "common/Date.hpp"
 #include "Nation.hpp"
 
-class Engine {
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+
+
+class Engine : public wxApp {
 private:
     time_point currentDate;
     Nation playerNation = Nation::testSetupSingleNation();
 public:
     Engine(time_point startingDate);
     void playTurn();
+    void setupGUI();
+
+    virtual bool OnInit();
 
     static Engine testSetup();
 };
