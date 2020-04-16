3cdfe1f5a0f690ed62cd98408b4dd5f7dd073351
Vincent Groff <>
2020-04-16 20:12:38 +0100

    revert wx widgets commit 2

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -1,30 +1,15 @@
 #pragma once
 
-#include <wx/wx.h>
 #include "common/Date.hpp"
 #include "Nation.hpp"
 
-class MyFrame : public wxFrame
-{
-public:
-    MyFrame();
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-};
-
-
-class Engine : public wxApp {
+class Engine {
 private:
     time_point currentDate;
     Nation playerNation = Nation::testSetupSingleNation();
 public:
     Engine(time_point startingDate);
     void playTurn();
-    void setupGUI();
-
-    virtual bool OnInit();
 
     static Engine testSetup();
 };
